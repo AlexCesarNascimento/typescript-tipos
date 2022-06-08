2c130d8912a9e99c6b2b72fbf7d28f4855cc5d97
Alex Cesar Nascimento <alexcesar.n@gmail.com>
2022-06-08 20:31:57 -0300

    chore(workspace): configurações de eslint

---
+++ typescript/.prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    trailingComma: 'all',   // para colocar virgula no final de cada linha de um objeto
+    singleQuote: true,      // para colocar aspas simples
+    printWidth: 80,         // para colocar o tamanho maximo de uma linha
+    tabWidth: 2,            // para colocar o tamanho de uma tab
+    endOfLine: 'auto',      // para colocar o final de linha automatico
+};
---
+++ typescript/ReadMe.md
@@ -0,0 +1,19 @@
+### Inicie um packge.json
+`npm init -y`
+
+### Instalando o Typescript
+`npm i typescript -D`
+`npm install --save @types/node`
+
+### Code Runner para Typescript
+`npm i ts-node -D`
+
+### EsLInt
+`npm i @typescript-eslint/eslint-plugin @typescript-eslint/parser -D`
+
+### Prettier
+`npm i prettier eslint-config-prettier eslint-pluggin-prettier -D`
+
+### tsc
+`npx tsc --init`
+
