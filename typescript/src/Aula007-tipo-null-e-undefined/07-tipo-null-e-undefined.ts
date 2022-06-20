b0a75dc53c01f9782a8c1cf23bbdfc63698b05b8
Alex Cesar Nascimento <alexcesar.n@gmail.com>
2022-06-19 23:26:31 -0300

    feat(aula07): tipo null e undefined

---
+++ typescript/src/Aula007-tipo-null-e-undefined/07-tipo-null-e-undefined.ts
@@ -0,0 +1,31 @@
+let x;
+
+if (typeof x === 'undefined') x = 20;
+
+console.log(x * 2);
+
+export function createPerson(
+  firstName: string,
+  lastName?: string,
+): {
+  firstName: string;
+  lastName?: string;
+} {
+  return {
+    firstName,
+    lastName,
+  };
+}
+
+export function squareOf(x: any) {
+  if (typeof x === 'number') return x * x;
+  return null;
+}
+
+const squareOfTwoNumber = squareOf(2);
+
+if (typeof squareOfTwoNumber === null) {
+  console.log('consta invalida');
+} else {
+  console.log(squareOfTwoNumber);
+}
